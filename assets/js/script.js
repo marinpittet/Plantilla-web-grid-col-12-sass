ea25977934ccea866be4d13b70e557f3b12b191a
andresMP <marinpittet@gmail.com>
2018-06-29 16:03:39 -0400

    minor fixes and new transitions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,32 @@ $(function(){
 
 		$('.sidenav--close').addClass('sidenav--open');
 
+
 		var sidewidth = $('.sidenav--open').css('width');
 
+
 		if (sidewidth === '0px') {
+			$('.navbar__toggle .fa-bars').animate({
+				opacity: 0
+			},100);
 			$('.sidenav--open').css('width', '100%');
 			$('.navbar__toggle .fa-bars').css('display', 'none');
 			$('.navbar__toggle .fa-times').css('display', 'block');
+			$('.navbar__toggle .fa-times, .sidenav__items').animate({
+				opacity: 1
+			},100);
 		} else {
+			$('.navbar__toggle .fa-times').animate({
+				opacity: 0
+			},100);
+			$('.sidenav__items').css('opacity', '0');
 			$('.sidenav--open').css('width', '0px');
-			$('.navbar__toggle .fa-bars').css('display', 'block');
 			$('.navbar__toggle .fa-times').css('display', 'none');
+			$('.navbar__toggle .fa-bars').css('display', 'block');
+			$('.navbar__toggle .fa-bars').animate({
+				opacity: 1
+			},100);
 		}
-				console.log(sidewidth);
 
 	})
 });
